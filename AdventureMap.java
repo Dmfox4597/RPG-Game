d461b43e2e8ef8de52db0626c64af0bc2a9579e6
Dmfox4597 <132308015+Dmfox4597@users.noreply.github.com>
2023-05-03 18:11:28 -0400

    Update AdventureMap.java

--- AdventureMap.java
+++ AdventureMap.java
@@ -1,21 +1,28 @@
 import java.util.HashMap;
 
-public class AdventureMap {
-
+public class AdventureMap
+{
     HashMap<String, Room> map;
 
     public AdventureMap() {
         map = new HashMap<>();
-
     }
 
-    //adds a room to the hashmap
+    /**
+     * Adds a room to the AdventureMap
+     * The name associated in the room must be lowercase
+     * @param room Room to be added to the AdventureMap
+     */
     public void addRoom(Room room) {
-        map.put(room.roomGetName().toLowerCase(), room);
+        map.put(room.getRoomName().toLowerCase(), room);
     }
 
-    //returns the room from the hashmap
+    /**
+     * Returns the Room assocaited with the string name given
+     * @param roomName the name of the room to be returned, room name must be valid
+     * @return the Room object assocaited with the name, the name will be in lowercase
+     */
     public Room getRoom(String roomName) {
-        return map.get(roomName.toLowerCase());
+        return map.get(roomName);
     }
 }
